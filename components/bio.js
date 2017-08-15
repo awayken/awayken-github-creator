fb47246ff30b8d2670f31ca59e2b1009d43fdfb0
Miles Rausch <miles@milesrausch.com>
2017-08-14 23:01:36 -0500

    Pull Bio copy into Bio component

---
+++ components/bio.js
@@ -0,0 +1,20 @@
+import Panel from './panel'
+
+export default () => (
+    <Panel>
+        <h1>Bio</h1>
+
+        <p>Hi, there. I'm Miles, a web developer from Sioux Falls, South Dakota.</p>
+
+        <p>I'm <a href="http://miles.ink" rel="noopener" title="Miles.ink, my writing website.">a writer for people</a>, and <a href="https://github.com/awayken" rel="noopener" title="My GitHub account.">a writer for computers</a>. I seemed destined to spend my time at a keyboard.</p>
+
+        <p>In my work time, I develop for both server (in CFML using <a href="http://lucee.org" rel="noopener">the Lucee engine</a>) and client (where I try to be <a href="https://www.w3.org/wiki/The_principles_of_unobtrusive_JavaScript" rel="noopener">unobtrusive</a>, <a href="https://alistapart.com/article/semanticsinhtml5" rel="noopener">semantic</a> and <a href="http://cssnext.io" rel="noopener">responsibly future-facing</a>).
+        At the same time, I've been having a flirty affair with <a href="https://nodejs.org" rel="noopener">Node.js</a> and <a href="https://facebook.github.io/react/" rel="noopener">React</a>. We're very much in love, although I'm not quite ready to commit.</p>
+
+        <p>In my personal time, <a href="http://miles.ink/read/" rel="noopener">I write short stories and novels</a>. Fiction is as strong a passion for me as programming, and I have published in some online publications and some print magazines. So, I'm like a really legit writer who can write good and stuff.</p>
+
+        <p>I'm self-motivated and self-disciplined. My goal with every project is producing the best product for the end user. I try to achieve this by writing my own code, adhering to HTML standards and best practices, and constantly and tirelessly learning and growing my craft.</p>
+
+        <p>Also, I'm funny. (I mean, not so much right now, but usually.)</p>
+    </Panel>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,14 @@
 import Layout from '../components/layout'
 import Panel from '../components/panel'
 
+import Bio from '../components/bio'
+
 export default () => (
     <Layout>
-        <Panel invert>
-            <h1>awayken.com</h1>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </Panel>
-
         <Panel>
             <h1>awayken.com</h1>
         </Panel>
+
+        <Bio />
     </Layout>
 )
