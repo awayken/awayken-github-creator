ae986e63d0d1c065f32b919001cf8b117ea589c5
Miles Rausch <miles@milesrausch.com>
2017-10-28 10:50:02 -0500

    Add Workbox to the build task

--- next.config.js
+++ next.config.js
@@ -1,28 +1,7 @@
-const path = require('path')
-const workboxPlugin = require('workbox-webpack-plugin')
-
-const DIST_DIR = '.'
-
 module.exports = {
   exportPathMap: function () {
     return {
       "/": { page: "/" }
     }
-  },
-  webpack: (config) => {
-    config.plugins.push(
-      new workboxPlugin({
-        globDirectory: DIST_DIR, // This should maybe be the generated folder? Having pathing issues.
-        globPatterns: ['**/*.{html,js,css}'],
-        globIgnores: [
-          '.next/**',
-          'awayken.github.io/**',
-          'node_modules/**'
-        ],
-        swDest: './static/sw.js' // This needs to go here so Next picks it up and copies it
-      })
-    )
-
-    return config
   }
 }
