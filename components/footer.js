580eeee87cbd82204a472ad57c268f2d8ea4155d
Miles Rausch <miles@resolve.com>
2022-10-19 08:22:33 -0500

    Update some links

--- components/footer.js
+++ components/footer.js
@@ -2,17 +2,13 @@ const copyrightYear = new Date().getFullYear()
 
 export default ({ year }) => (
     <footer>
-        <section>
-            <p>&copy; {year ? year : copyrightYear} Miles Rausch</p>
+        <p>&copy; {year ? year : copyrightYear} Miles Rausch</p>
 
-            <nav>
-                <a href="https://stackoverflow.com/story/awayken">Resume</a>
-                <a href="http://www.milesrausch.com/">MilesRausch.com</a>
-                <a href="http://miles.ink/">Miles.ink</a>
-                <a href="https://twitter.com/awayken">Twitter/@awayken</a>
-                <a href="https://github.com/awayken">GitHub/awayken</a>
-            </nav>
-        </section>
+        <nav>
+            <a href="http://www.milesrausch.com/">MilesRausch.com</a>
+            <a href="http://miles.ink/">Miles.ink</a>
+            <a href="https://github.com/awayken">GitHub/awayken</a>
+        </nav>
 
         <style jsx>{`
             footer {
