47c4a019a0f44bce46e87c7afd9ca6b2eec67cc8
Miles Rausch <miles@resolve.com>
2022-10-19 08:35:23 -0500

    Update the bio

--- components/bio.js
+++ components/bio.js
@@ -4,16 +4,13 @@ export default () => (
 
         <p>Hi, there. I'm Miles, a web developer from Sioux Falls, South Dakota.</p>
 
-        <p>I'm <a href="http://miles.ink" rel="noopener noreferrer" title="Miles.ink, my writing website.">a writer for people</a>, and <a href="https://github.com/awayken" rel="noopener noreferrer" title="My GitHub account.">a writer for computers</a>. I seemed destined to spend my time at a keyboard.</p>
+        <p>I'm <a href="http://miles.ink" rel="noopener noreferrer" title="Miles.ink, my writing website.">a writer for people</a> and <a href="https://github.com/awayken" rel="noopener noreferrer" title="My GitHub account.">a writer for computers</a>. I seemed destined to spend my time at a keyboard.</p>
 
-        <p>In my work time, I develop for both server (in CFML using <a href="http://lucee.org" rel="noopener noreferrer">the Lucee engine</a>) and client (where I try to be <a href="https://www.w3.org/wiki/The_principles_of_unobtrusive_JavaScript" rel="noopener noreferrer">unobtrusive</a>, <a href="https://alistapart.com/article/semanticsinhtml5" rel="noopener noreferrer">semantic</a> and <a href="http://cssnext.io" rel="noopener noreferrer">responsibly future-facing</a>).
-        At the same time, I've been having a flirty affair with <a href="https://nodejs.org" rel="noopener noreferrer">Node.js</a> and <a href="https://facebook.github.io/react/" rel="noopener noreferrer">React</a>. We're very much in love, although I'm not quite ready to commit.</p>
+        <p>In my work time, I lead a team of frontend developers where we're building user interfaces using <a href="https://angular.io/" rel="noopener noreferrer">Angular</a>.</p>
 
-        <p>In my personal time, <a href="http://miles.ink/read/" rel="noopener noreferrer">I write short stories and novels</a>. Fiction is as strong a passion for me as programming, and I have published in some online publications and some print magazines. So, I'm like a really legit writer who can write good and stuff.</p>
+        <p>In my personal time, <a href="http://miles.ink/read/" rel="noopener noreferrer">I write poetry, short stories and novels</a>. I'm like a really legit writer who can write good and stuff.</p>
 
         <p>I'm self-motivated and self-disciplined. My goal with every project is producing the best product for the end user. I try to achieve this by writing my own code, adhering to HTML standards and best practices, and constantly and tirelessly learning and growing my craft.</p>
-
-        <p>Also, I'm funny. (I mean, not so much right now, but usually.)</p>
 
         <style jsx>{`
             a,
